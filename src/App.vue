b267513e5a39b0d5b415f7913495f40dfe076ca2
jgq-cell <17318646019@163.com>
2023-05-26 22:02:45 +0800

    feat: 列表编辑功能实现

--- src/App.vue
+++ src/App.vue
@@ -14,13 +14,18 @@ onMounted(() => getList())
 // TODO: 删除功能
 // 获取当前行的id-> 通过id调用删除接口-> 更新最新的列表
 const onDelete = async (id) => {
-  console.log(id)
+  // console.log(id)
   // 调用删除接口
   await axios.delete(`/del/${id}`)
   // 刷新数据
   getList()
 }
 // TODO: 编辑功能
+// 打开一个弹框（获取子组件实例 调用方法或修改属性，一般定义方法来修改属性）-> 回填数据 -> 更新数据
+const editRef = ref(null)
+const onEdit = (row) => {
+  editRef.value.open(row)
+}
 </script>
 
 <template>
@@ -32,7 +37,7 @@ const onDelete = async (id) => {
       <el-table-column label="操作" width="150">
         <!-- 作用域插槽：row获取表格props信息 -->
         <template #default="{ row }">
-          <el-button type="primary" link>编辑</el-button>
+          <el-button type="primary" @click="onEdit(row)" link>编辑</el-button>
           <el-button type="danger" @click="onDelete(row.id)" link
             >删除</el-button
           >
@@ -40,7 +45,7 @@ const onDelete = async (id) => {
       </el-table-column>
     </el-table>
   </div>
-  <Edit />
+  <Edit ref="editRef" @on-update="getList" />
 </template>
 
 <style scoped>
